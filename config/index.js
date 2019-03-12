be1917f38b312def47ee7169b3cc7483693fd58d
hjn <l>
2019-03-12 13:53:46 +0800

    修复项目不能正常运行

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 'use strict'
 // Template version: 1.3.1
-// 
+// see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
 
@@ -21,12 +21,12 @@ module.exports = {
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
     proxyTable: {
       '/api':{
-       
-        target:"https://abc.com",//设置你调用的接口域名和端口号 别忘了加http
+        
+        target:"https://abc:9091",//设置你调用的接口域名和端口号 别忘了加http
         changeOrigin:true,
         // pathRewrite:{
         //   '^/api':''//这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 
-        //                 
+        //                
         // }
       }
     },
